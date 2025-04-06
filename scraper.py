4dadbbc58f208ef0bd326cfea03ae90b8c9f076f
Ruby Lu <rubylu12345@gmail.com>
2025-04-06 12:18:17 -0400

    get film details

--- scraper.py
+++ scraper.py
@@ -2,6 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+star_to_rating = {
+    "★": 1,
+    "★★": 2,
+    "★★★": 3,
+    "★★★★": 4,
+    "★★★★★": 5,
+    "½": 0.5,
+    "★½": 1.5,
+    "★★½": 2.5,
+    "★★★½": 3.5,
+    "★★★★½": 4.5
+}
+
 def get_rated_films(username):
     base_url = f"https://letterboxd.com/{username}/films/"
     films = []
@@ -19,7 +32,8 @@ def get_rated_films(username):
         
         if not film_list:
             break
-            
+        
+        # get ratings
         for film in film_list:
             title = film.find('img')['alt']
             film_url = film.find('div')['data-film-slug']
@@ -29,12 +43,36 @@ def get_rated_films(username):
             films.append({
                 'title': title,
                 'film_url': film_url,
-                'rating': rating
+                'rating': star_to_rating[rating]
             })
         
         page += 1
     
     return pd.DataFrame(films)
 
+def get_film_details(film_slug):
+    film_url = f"https://letterboxd.com/film/{film_slug}/"
+    response = requests.get(film_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    details = {}
+    
+    # get release year
+    year = soup.find('a', href=lambda x: x and '/year/' in x)
+    details['year'] = year.text.strip() if year else None
+    
+    # get genres
+    genres = []
+    genre_div = soup.find('div', id='tab-genres')
+    if genre_div:
+        genres = [a.text for a in genre_div.find_all('a', href=lambda x: x and '/films/genre/' in x)]
+    details['genres'] = genres
+    
+    # get director
+    director = soup.find('a', href=lambda x: x and '/director/' in x)
+    details['director'] = director.text if director else None
+    
+    return details
+
 films_df = get_rated_films('rubylu')
-print(films_df)
+# print(films_df)
